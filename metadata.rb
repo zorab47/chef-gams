a853ba95c54955f6e20ee78fc1f3936d71e96957
Charles Maresh <charles.maresh@orm-tech.com>
2014-01-27 13:39:44 -0600

    Setup for initial v0.1.0 release

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,4 @@ maintainer_email "charles.maresh@orm-tech.com"
 license          "Apache 2.0"
 description      "Installs and configures GAMS. See http://gams.com."
 
-version          "0.1"
+version          "0.1.0"
